98cb76d1e9ca25b7505253ea981b3c0f817aba86
Diego <diegoalejandrogarcia140902@gmail.com>
2024-03-17 14:29:28 -0500

    Se configuraron correctamente las migraciones, y se hizo la primera parte de la logica para el registro de usuarios

--- API/database/migrations/2024_03_17_191251_create_user_posts_table.php
+++ API/database/migrations/2024_03_17_191251_create_user_posts_table.php
@@ -11,24 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
             $table->timestamps();
-            
-           
+    
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('post_id')->references('post_id')->on('posts')->onDelete('cascade');
         });
     }
+    
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_posts');
     }
 };
